5c9191293c5769345872cd27e9e0412b626a386d
SebMata <sebastian.matasalazar@ucr.ac.cr>
2022-05-03 20:24:07 -0600

    Ejecución del main, el vector y la suma total

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "TipoEnvio.h"
 #include "EnvioPostal.h"
+#include "EnvioFedex.h"
 #include <iostream>
 #include <vector>
 
@@ -7,6 +8,21 @@ using namespace std;
 
 int main(){
 
-    vector<TipoEnvio *> envio;
+    vector<TipoEnvio *> envios;
+
+    EnvioPostal* envio1 = new EnvioPostal(5,14,2);
+    envios.push_back(envio1);
+
+    EnvioFedex* envio2 = new EnvioFedex(7,720);
+    envios.push_back(envio2);
+
+    double totalMontoEnvios = 0;
+
+    for (TipoEnvio* TipoEnvio : envios)
+    {
+        totalMontoEnvios += TipoEnvio->CalculoEnvio();
+    }
+
+    cout << "El monto total de los envíos es de " << totalMontoEnvios << endl;
      
 }
